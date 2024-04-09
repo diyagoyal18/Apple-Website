a247fb0ad4926a7dc6c53741f5ffd188fe561ad1
Diya Goyal <dgoyal6_be22@thapar.edu>
2024-04-09 23:55:16 +0530

    model modifications

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,21 +1,41 @@
-import { PerspectiveCamera, View } from "@react-three/drei"
+import { OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
 import Lights from './Lights'
 import { Suspense } from "react"
 import IPhone from './IPhone'
+import * as THREE from 'three'
 const ModelView = ({index, groupRef, gsapType, controlRef, setRotationSize, size, item}) => {
   return (
     
       <View index={index}
       id={gsapType}
-      className={`border-2 border-red-500 w-full h-full ${index === 2 ? 'right-[-100%]' : ''}`}
+      className={` w-full h-full ${index === 2 ? 'right-[-100%]' : ''}`}
       >
         <ambientLight intensity={0.3}/>
         <PerspectiveCamera makeDefault position={[0,0,4]}/> 
         <Lights/>
+      
+        <OrbitControls
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0,0,0)}
+        onEnd={()=>setRotationState(controlRef.curr
+        .getAzimuthalAngle())}
+
+        /> 
+        <group ref={groupRef} name={`${index===1}? 'small' : 'large'`} position={[0,0,0]}>
+
         <Suspense fullback={<div>Loading</div>}>
 
         </Suspense>
-        <IPhone/>  
+        </group>
+        <IPhone
+        scale= {index===1? [15,15,15]: [17,17,17]}
+        item={item}
+        size={size}
+        />  
       </View>
       )
 }
